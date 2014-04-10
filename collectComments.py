97a5b9f40db88d009267746b08a849ab7f716b73
Spurthi <spurthi@spurthi-HP-Pavilion-dv2500-Notebook-PC.(none)>
2014-04-10 01:20:22 -0400

    Get processed comments

---
+++ collectComments.py
@@ -0,0 +1,71 @@
+from nltk import word_tokenize,sent_tokenize
+import cPickle as pickle
+
+def isNum(n):
+	try:
+		n=float(n)
+		return float(n)
+	except:
+		return -1
+
+def getProcessedComments(movieNames,commentsList,unwantedSyms):
+	n=0
+	comments=dict()
+	for movieComments in commentsList:
+		processedComments=[]
+		string=''
+		for sent in movieComments:
+			for word in word_tokenize(sent):
+				word=word.strip(' ')
+				flag=0
+				for sym in unwantedSyms:
+					if sym in word:
+						flag=1
+						break
+				if flag==0:
+					string+=word+' '
+		processedComments.append(string)
+		comments[movieNames[n]]=processedComments
+		n=n+1
+	return comments
+
+
+
+def getPickledFile(comments,fname):
+	pickle.dump(comments,open(fname,'wb'))
+	
+
+def main(fname):
+	with open(fname,'r') as f:
+		data=f.readlines()		
+		combinedComments=[]
+		comments=[]
+		for line in data:
+			if "$$$$" in line:
+				comments.append(combinedComments)
+				combinedComments=[]
+			else:
+				combinedComments.append(line)
+
+	return comments
+
+if __name__=="__main__":
+	commentsList=main('comments20.txt')
+	
+	unwantedSyms=['?','(',')',',',':',';','{','}','[',']','\\','\'','\"','.','!','@','#','$','%','^','&','*','/','+','-','_','|','~','`','=']
+
+	with open('movies20.txt','r') as f:
+		names=f.readlines()
+		movieNames=[]
+		for name in names:
+			name=name.strip('\n')
+			movieNames.append(name)
+	
+	comments=getProcessedComments(movieNames,commentsList,unwantedSyms)
+	print comments
+
+
+	getPickledFile(comments,'comments20.p')
+
+
+	#print videoStats
